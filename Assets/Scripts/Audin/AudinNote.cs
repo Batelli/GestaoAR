5086afe78953bb7a1e4d4524a67770bee5ff6794
Batelli <batelli@gmail.com>
2024-04-22 16:31:14 -0300

    Audin 100% Implemented

--- Assets/Scripts/Audin/AudinNote.cs
+++ Assets/Scripts/Audin/AudinNote.cs
@@ -1,11 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class AudinNote : MonoBehaviour
 {
+    public TMP_Text audinCode;
+    public TMP_Text[] audinQ;
+
     public void LoadNote(string keyCode)
     {
+        audinCode.text = "Caso: " + keyCode.ToUpper();
 
+        string[] str = AudinQuestionTranslator.Instance.GetAllAnswerText(keyCode);
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (i >= audinQ.Length)
+                return;
+
+            audinQ[i].text = str[i];
+        }
     }
 }
